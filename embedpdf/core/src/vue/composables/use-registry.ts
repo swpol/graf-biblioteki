45f37571f56982523769c947f94af653cd10a748
swpol <swierzbicki@term-service.pl>
2025-07-22 23:53:19 +0200

    Delete embedpdf directory

--- embedpdf/core/src/vue/composables/use-registry.ts
+++
@@ -1,8 +0,0 @@
-import { inject } from 'vue';
-import { pdfKey } from '../context';
-
-export function useRegistry() {
-  const ctx = inject(pdfKey);
-  if (!ctx) throw new Error('useRegistry must be used inside <EmbedPDF>');
-  return ctx;
-}
