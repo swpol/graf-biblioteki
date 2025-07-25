3c9142d08b0ca7b0db9c8787ae8ec5a39df53423
swpol <swierzbicki@term-service.pl>
2025-07-25 10:43:24 +0200

    Delete embedpdf/pdfium/1.0.11/dist directory

--- embedpdf/pdfium/1.0.11/dist/index.cjs.d.ts
+++
@@ -1,3 +0,0 @@
-import { type PdfiumModule, type WrappedPdfiumModule } from './base';
-export type { PdfiumModule, PdfiumRuntimeMethods, Type, CWrappedFunc, NameToType, NamesToType, Functions, Wrapped, Methods, WrappedPdfiumModule, } from './base';
-export declare function init(moduleOverrides: Partial<PdfiumModule>): Promise<WrappedPdfiumModule>;
