d7bdb3b0386a0b430882e405c82efea65e76b130
swpol <swierzbicki@term-service.pl>
2025-07-25 10:42:49 +0200

    Delete embedpdf/core/1.0.11/dist directory

--- embedpdf/core/1.0.11/dist/preact/hooks/use-store-state.d.ts
+++
@@ -1,6 +0,0 @@
-import { CoreState, StoreState } from '../..';
-/**
- * Hook that provides access to the current global store state
- * and re-renders the component when the state changes
- */
-export declare function useStoreState<T = CoreState>(): StoreState<T> | null;
